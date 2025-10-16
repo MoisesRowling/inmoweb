b1b82ae4f2d78d082b04b12ffce24708d7dc3423
Moises Ruiz Salinas <moisesruizsalinas@gmail.com>
2025-10-16 05:59:34 +0000

    sigue el mismo error

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -2,6 +2,8 @@
 
 
 
+
+
 import { NextResponse, type NextRequest } from 'next/server';
 import type { Investment, Property, Transaction, User } from '@/lib/types';
 import { readDB, writeDB } from '@/lib/db';
